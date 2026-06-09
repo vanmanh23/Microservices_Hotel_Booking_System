488a88474417c88d5a76bd8f4c78cbd995e11605
vanmanh23 <manhcan03@gmail.com>
2026-06-09 20:54:28 +0700

    refactor: optimize Dockerfile using multi-stage build

--- hotel-service/src/test/java/com/manhnguyen/hotel/integration/HotelServiceIntegration.java
+++ hotel-service/src/test/java/com/manhnguyen/hotel/integration/HotelServiceIntegration.java
@@ -1,2 +1,8 @@
-package com.manhnguyen.hotel.integration;public class HotelServiceIntegration {
+package com.manhnguyen.hotel.integration;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class HotelServiceIntegration {
+
 }
